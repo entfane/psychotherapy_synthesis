b14603b4429e1a1a5a39817fb50f10928f3cac4d
entfane <nik77kez@gmail.com>
2025-05-31 18:12:11 +0200

    Added gemini as another response generator

--- response_generator.py
+++ response_generator.py
@@ -1,4 +1,6 @@
 from openai import OpenAI
+from google import genai
+from google.genai import types
 from dotenv import load_dotenv
 
 
@@ -7,11 +9,22 @@ class ResponseGenerator:
     def __init__(self):
         load_dotenv()
         self.openai_client = OpenAI()
+        self.gemini_client = genai.Client()
 
-    def generate_response(self, system_prompt, user_prompt):
+    def generate_openai_response(self, system_prompt, user_prompt):
         response = self.openai_client.responses.create(
             model="gpt-4o-mini",
             instructions=system_prompt,
             input=user_prompt
         )
         return response.output_text
+    
+    def generate_gemini_response(self, system_prompt, user_prompt):
+        response = self.gemini_client.models.generate_content(
+            model="gemini-2.0-flash-001",
+            contents=user_prompt,
+            config=types.GenerateContentConfig(
+                system_instruction=system_prompt,
+            ),
+        )
+        return response.text
